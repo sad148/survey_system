ab593e2c3ac00329cd6e142878a6b869cb6a0a46
Saurabh Dhamnaskar <0411ameya@gmail.com>
2017-12-23 10:32:06 -0500

    fix issue of logging in on clicking register button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -65,23 +65,27 @@ class Login extends Component {
         const { getFieldDecorator } = this.props.form;
         return (
             <div id = 'login'>
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <FormItem>
+                <Form className="login-form">
+                    <FormItem
+                        label="Username"
+                    >
                         {getFieldDecorator('username', {
                             rules: [{ required: true, message: 'Please input your username!' }],
                         })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                            <Input id = 'username' prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}/>
                         )}
                     </FormItem>
-                    <FormItem>
+                    <FormItem
+                        label="Password"
+                    >
                         {getFieldDecorator('password', {
                             rules: [{ required: true, message: 'Please input your Password!' }],
                         })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                            <Input id ='password' prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password"/>
                         )}
                     </FormItem>
                     <FormItem>
-                        <Button id = 'signin' type="primary" htmlType="submit" className="login-form-button">
+                        <Button id = 'signin' onClick = {this.handleSubmit} type="primary" htmlType="submit" className="login-form-button">
                             Log in
                         </Button>
                         <br />
