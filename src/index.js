c3514d2188f025584525a2574a710bd8b4f2d1e5
Saurabh Dhamnaskar <0411ameya@gmail.com>
2018-01-21 12:58:16 -0500

    install react app rewired for icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './iconfont.css'
 import App from './App';
 import { Provider } from 'react-redux'
 import store, { history } from './store.js'
