d7fe7445c10a5a999d43157007a63213a99ec851
Saurabh Dhamnaskar <0411ameya@gmail.com>
2018-01-29 19:43:38 -0500

    add action menu and responses column

--- src/components/ListProjectData.js
+++ src/components/ListProjectData.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import { Table } from 'antd';
-import '../../node_modules/antd/lib/table/style/index.css'
-import '../../node_modules/antd/lib/pagination/style/index.css'
+import React, {Component} from 'react';
+import {Table, Button, Popover, Icon, Divider, Modal, Input} from 'antd';
 
 const columns = [{
     title: 'Project Name',
@@ -9,12 +7,16 @@ const columns = [{
 }, {
     title: 'Created on',
     dataIndex: 'createdAt',
-},{
-    title:'Action',
-    dataIndex:'action'
+}, {title: 'Responses', dataIndex: 'responses'}, {
+    title: 'Action',
+    dataIndex: 'action'
 }];
 
-class ListProjectData extends Component{
+class ListProjectData extends Component {
+    state = {
+        visible: false,
+        input: ""
+    }
     componentWillMount = () => {
         this.formData(this.props.projectData)
     }
@@ -22,15 +24,51 @@ class ListProjectData extends Component{
         this.formData(nextProps.projectData)
     }
 
+    handleCancel = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    sharelink = (data) => {
+        let baseUrl = `${window.location.hostname}:3000/survey_system/questions/${data}`;
+        let input = (<Input value={baseUrl}></Input>);
+        this.setState({link: input, visible: true})
+    }
+
     formData = (data) => {
-        //let tableData = []
-        this.setState({tableData:data.projects})
+        data.projects.map((item) => {
+            let content = (
+                <div style={{width: "100px"}}>
+                    <p style={{cursor: "pointer"}} onClick={() => this.sharelink(item.projectId)}>Share <Icon
+                        type="share-alt"/>
+                    </p>
+                    <Divider/>
+                    <p style={{cursor: "pointer"}}>Clone <Icon type="copy"/></p>
+                    <Divider/>
+                    <p style={{cursor: "pointer"}}>Edit <Icon type="edit"/></p>
+                </div>
+            );
+            return item.action = (<Popover content={content} placement="bottomLeft">
+                <Button><Icon type="ellipsis"/></Button>
+            </Popover>)
+        })
+
+        this.setState({tableData: data.projects})
     }
 
     render = () => {
         return (
             <div>
                 <Table columns={columns} dataSource={this.state.tableData}/>
+                <Modal
+                    title="Link"
+                    visible={this.state.visible}
+                    footer={null}
+                    onCancel={this.handleCancel}
+                >
+                    {this.state.link}
+                </Modal>
             </div>
         )
     }
