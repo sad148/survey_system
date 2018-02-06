9f9e33667aa92f680a3143e5bf1754ea4218f84b
Saurabh Dhamnaskar <0411ameya@gmail.com>
2018-02-06 13:04:10 -0500

    design complete for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,36 +1,34 @@
 import React, {Component} from 'react';
 import RegisterUser from '../components/RegisterUser.js';
 import login from '../actions/Login.js';
+import {Layout} from 'antd';
 import {connect} from 'react-redux';
-import { browserHistory } from 'react-router';
-import {withRouter} from 'react-router-dom'
-import { history } from '../store';
+import {browserHistory} from 'react-router';
 import Error from '../components/Error.js'
-import '../../node_modules/antd/lib/input/style/index.css';
-import '../../node_modules/antd/lib/button/style/index.css';
-import '../../node_modules/antd/lib/form/style/index.css';
-import '../../node_modules/antd/lib/modal/style/index.css';
-import { Form, Icon, Input, Button, Modal} from 'antd';
+import {Form, Icon, Input, Button, Modal, Card} from 'antd';
+
+const {Header, Footer, Sider, Content} = Layout;
+
 const FormItem = Form.Item;
 
 class Login extends Component {
     componentWillMount = () => {
         this.setState({
-            showRegister:false,
-            visible:false
+            showRegister: false,
+            visible: false
         })
     }
 
     componentWillReceiveProps = (nextProps) => {
-        if(nextProps.loginSuccess == true) {
-            this.props.dispatch({type:"RESET_LOGIN"})
+        if (nextProps.loginSuccess == true) {
+            this.props.dispatch({type: "RESET_LOGIN"})
             browserHistory.replace('/survey_system/home');
-            this.props.dispatch({type:"PROJECT_DATA", payload:nextProps.data});
+            this.props.dispatch({type: "PROJECT_DATA", payload: nextProps.data});
         }
     }
 
     shouldComponentUpdate = (nextProps) => {
-        if(nextProps.loginSuccess == false && this.props.loginSuccess == true)
+        if (nextProps.loginSuccess == false && this.props.loginSuccess == true)
             return false;
         return true;
     }
@@ -61,51 +59,71 @@ class Login extends Component {
     }
 
     render = () => {
-        const { getFieldDecorator } = this.props.form;
+        const {getFieldDecorator} = this.props.form;
         return (
-            <div id = 'login'>
-                <Form className="login-form">
-                    <FormItem
-                        label="Email"
-                    >
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Please input your email' }],
-                        })(
-                            <Input id = 'username' prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}/>
-                        )}
-                    </FormItem>
-                    <FormItem
-                        label="Password"
-                    >
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input id ='password' prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password"/>
-                        )}
-                    </FormItem>
-                    <FormItem>
-                        <Button id = 'signin' onClick = {this.handleSubmit} type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                        </Button>
-                        <br />
-                        <Button id = 'register' type="primary" onClick = {this.showModal} htmlType="submit" className="login-form-button">
-                            Register
-                        </Button>
-                    </FormItem>
-                </Form>
-                <Modal title = 'Register' visible = {this.state.visible} footer = {null} onCancel = {this.handleCancel}><RegisterUser/></Modal>
-                <Error/>
+            <div id='loginContainer'>
+                <div id="titleContainer">
+                    <div id="title1">
+                        PITT Usability Questionnaire
+                    </div>
+                    <div id="title2">
+                        For Telehealth System (TUQ) and Mobile Health Apps (MAUQ)
+                    </div>
+                </div>
+                <div id="loginFormContainer">
+                    <Form className="login-form">
+                        <div id="loginTextContainer">
+                        <label style={{fontSize: "50px"}} id="loginText">Login</label>
+                        </div>
+                        <FormItem>
+                            {getFieldDecorator('username', {
+                                rules: [{required: true, message: 'Please input your email'}],
+                            })(
+                                <div>
+                                    <label style={{color: "red"}}>*</label>&nbsp;<label>E-mail</label>
+                                    <Input id='username' className="form-input" autoComplete="off"
+                                           prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}/>
+                                </div>
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{required: true, message: 'Please input your Password!'}],
+                            })(
+                                <div>
+                                    <label style={{color: "red"}}>*</label>&nbsp;<label>Password</label>
+                                    <Input id='password' className="form-input"
+                                           prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                           type="password"/>
+                                </div>
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            <div id={"loginPageButtons"}>
+                                <input type="button" id='forgotPwd' onClick={this.handleSubmit} value="Forgot Password" />
+                                <input type="button" id='signin' onClick={this.handleSubmit} value="Continue" />
+                                <br/>
+                            </div>
+                        </FormItem>
+                        <div id="registerLinkDiv">
+                            <label id="registerLink">Not registered,<a style={{color:"#e3f4fd"}} onClick={this.showModal}>CLICK
+                                HERE</a></label>
+                        </div>
+                    </Form>
+                </div>
             </div>
+
         );
     }
 }
+
 const mapStateToProps = (store) => {
     return {
-        loginSuccess:store.login.loginSuccess,
-        data:store.login.data,
-        message:store.login.message
+        loginSuccess: store.login.loginSuccess,
+        data: store.login.data,
+        message: store.login.message
     }
 }
 
 const WrappedNormalLoginForm = Form.create()(Login);
-export default connect (mapStateToProps)(WrappedNormalLoginForm);
+export default connect(mapStateToProps)(WrappedNormalLoginForm);
