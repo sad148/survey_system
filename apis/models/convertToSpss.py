8b82b8d5dcb19c3ce116f72d03b5791fb0f030de
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-03-05 12:32:31 -0500

    integrate js and python to export spss

---
+++ apis/models/convertToSpss.py
@@ -0,0 +1,14 @@
+import sys, json
+from savReaderWriter.savWriter import *;
+
+file1 = open("./data.json")
+data = json.loads(file1.read())
+savFileName = "./exports/" + sys.argv[1];
+varNames = data["varNames"]["varNames"];
+records = data["records"]["records"];
+varTypes = data["varTypes"]
+varLabels = data["varLabel"]
+measureLevels = data["measureLevels"]
+with SavWriter(savFileName, varNames, varTypes, None, varLabels, None, None, measureLevels) as writer:
+    for record in records:
+        writer.writerow(record)
