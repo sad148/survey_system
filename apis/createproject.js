6d9dcc201b76900ff313b02e17e65a83fac4e789
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-03-19 23:42:10 -0400

    initialize empty response count and latest entry date

--- apis/createproject.js
+++ apis/createproject.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v1');
+
 function createproject(req, db, cb) {
     let data = req.body.data;
     let userid = req.body.data.userid;
@@ -9,52 +10,54 @@ function createproject(req, db, cb) {
     let projectId = uuid().split("-").join("");
     step1.id = projectId
     let steps = [];
-    for(let i = 0;i < step2.length;i++) {
+    for (let i = 0; i < step2.length; i++) {
         step2[i].stepNum = "step2"
         step2[i].type = "radio"
         delete step2[i].select;
         step2[i].options = []
-        for(let j = 1;j <= step2[i].limit;j++) {
+        for (let j = 1; j <= step2[i].limit; j++) {
             step2[i].options.push(j);
         }
         steps.push(step2[i]);
     }
 
-    for(let i = 0;i < step3.length;i++) {
+    for (let i = 0; i < step3.length; i++) {
         step3[i].stepNum = "step3"
         steps.push(step3[i]);
     }
 
-    for(let i = 0;i < step4.length;i++) {
+    for (let i = 0; i < step4.length; i++) {
         step4[i].stepNum = "step4"
         steps.push(step4[i]);
     }
 
     let projectData = {
-        userid:userid,
-        projectId:projectId,
-        projectName:step1.project_name,
-        createdAt:new Date()
+        userid: userid,
+        projectId: projectId,
+        projectName: step1.project_name,
+        createdAt: new Date(),
+        response: 0,
+        latestEntry: ""
     }
 
     db.collection("projects").insertOne(projectData)
         .then((res) => {
-        db.createCollection(projectId + "::details", {autoIndexId:false})
-            .then((res) => {
-            db.collection(projectId + "::details").insertMany(steps)
+            db.createCollection(projectId + "::details", {autoIndexId: false})
                 .then((res) => {
-                    cb({
-                        code:200,
-                        message:"Project created successfully"
-                    })
+                    db.collection(projectId + "::details").insertMany(steps)
+                        .then((res) => {
+                            cb({
+                                code: 200,
+                                message: "Project created successfully"
+                            })
+                        })
+                        .catch((err) => {
+                            console.log("insert err", err);
+                        })
                 })
                 .catch((err) => {
-                    console.log("insert err", err);
+                    console.log("err", err);
                 })
-            })
-            .catch((err) => {
-                console.log("err", err);
-            })
         })
         .catch((err) => {
 
