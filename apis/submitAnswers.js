897179ada2a4cfb27a153e97307c3d7d9eb3ed55
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-03-19 23:42:30 -0400

    increment response count and update latest entry date

--- apis/submitAnswers.js
+++ apis/submitAnswers.js
@@ -5,21 +5,52 @@ function submitAnswers(req, db, cb) {
     let surveyId = req.body.data.surveyId;
     let submittedTime = moment().format();
     let submittedEpoch = new Date().getTime();
+
     for (let i = 0; i < answers.length; i++) {
         answers[i].submittedTime = submittedTime
         answers[i].submittedEpoch = submittedEpoch
     }
+
+    const data = db.collection('projects');
+
     db.createCollection(surveyId + "::answers", {autoIndexId: false})
         .then((res) => {
             db.collection(surveyId + "::answers").insertMany(answers)
                 .then((res) => {
-                    cb({
-                        code: 200,
-                        message: "Answers submitted successfully"
-                    })
+                    data.findOne({projectId: surveyId})
+                        .then((projectDetails) => {
+                            data.findOneAndUpdate({
+                                projectId: surveyId
+                            }, {
+                                $set: {
+                                    response: !projectDetails.response ? 1 : parseInt(projectDetails.response) + 1,
+                                    latestDateEntry: submittedTime
+                                }
+                            }, {
+                                returnOriginal: false,
+                                upsert: true
+                            })
+                                .then((success) => {
+                                    cb({
+                                        code: 200,
+                                        message: "Answers submitted successfully"
+                                    })
+                                })
+                                .catch((err) => {
+                                    cb({
+                                        code: 400,
+                                        message: "Error in submitting answers"
+                                    })
+                                })
+                        })
+                        .catch((err) => {
+                            cb({
+                                code: 400,
+                                message: "Error in submitting answers"
+                            })
+                        })
                 })
                 .catch((err) => {
-                    console.log(err);
                     cb({
                         code: 400,
                         message: "Error in submitting answers"
