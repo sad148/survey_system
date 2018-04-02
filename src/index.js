1168287e8a06b741b1bba2d70c65ef33bacd3aaf
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-02 02:32:14 -0400

    remove unwanted files
    remove unwanted files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux'
 import store, { history } from './store.js'
 import Login from './pages/Login.js'
