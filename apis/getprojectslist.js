07f7c04e36e4dfc6dd03657b1a453d00144a13f3
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-18 11:40:57 -0400

    show all projects on refreshing homepage

--- apis/getprojectslist.js
+++ apis/getprojectslist.js
@@ -1,34 +1,69 @@
 function getprojectslist(req, db, cb) {
     let data = req.body
     let userid = data.userid;
+    let username = data.username
     const projectsData = db.collection('projects');
-    projectsData.find({userid: userid}, {sort: {'createdAt': -1}}).toArray(function (err, projectsResp) {  //-1 in sort query signifies descending order
-        if (err) {
-            console.log(err);
-            cb({
-                code: 400,
-                message: "Error in getting list of projects"
-            })
-        } else {
-            if (projectsResp.length == 0) {
+    if (username === "admin") {
+        projectsData.find({}, {sort: {'createdAt': -1}}).toArray(function (err, projectsResp) {
+            if (err) {
+                console.log(err);
                 cb({
-                    code: 200,
-                    message: "No projects created yet",
-                    data: {
-                        projects: []
-                    }
+                    code: 400,
+                    message: "Error in getting list of projects"
                 })
             } else {
-                let response = {
-                    code: 200,
-                    data: {
-                        projects: projectsResp
+
+                if (projectsResp.length == 0) {
+                    cb({
+                        code: 200,
+                        message: "No projects created yet",
+                        data: {
+                            admin: true,
+                            projects: []
+                        }
+                    })
+                } else {
+                    let response = {
+                        code: 200,
+                        data: {
+                            admin: true,
+                            projects: projectsResp
+                        }
                     }
+                    cb(response);
                 }
-                cb(response);
             }
-        }
-    })
+        });
+    }
+    else {
+        projectsData.find({userid: userid}, {sort: {'createdAt': -1}}).toArray(function (err, projectsResp) {  //-1 in sort query signifies descending order
+            if (err) {
+                console.log(err);
+                cb({
+                    code: 400,
+                    message: "Error in getting list of projects"
+                })
+            } else {
+                if (projectsResp.length == 0) {
+                    cb({
+                        code: 200,
+                        message: "No projects created yet",
+                        data: {
+                            projects: []
+                        }
+                    })
+                } else {
+                    let response = {
+                        code: 200,
+                        data: {
+                            projects: projectsResp
+                        }
+                    }
+                    cb(response);
+                }
+            }
+        })
+    }
 }
 
 module.exports.getprojectslist = getprojectslist;
--- src/actions/GetProjectsList.js
+++ src/actions/GetProjectsList.js
@@ -2,11 +2,12 @@ var request = require('superagent');
 
 export default function getprojectslist(userid) {
     return function (dispatch) {
+        let username = sessionStorage.getItem("email");
         let apiUrl = sessionStorage.getItem("apiurl");
         request
             .post(apiUrl + 'getprojectslist')
             .set('Content-Type', 'application/json')
-            .send({userid: userid})
+            .send({userid: userid, username: username})
             .end((err, res) => {
                 if (err) {
                     console.log("Error", err);
