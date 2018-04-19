7a7313690e402b93bad04b0eb7a3030895e751b0
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-18 21:50:12 -0400

    handle empty answers submitted

--- apis/submitAnswers.js
+++ apis/submitAnswers.js
@@ -5,14 +5,21 @@ function submitAnswers(req, db, cb) {
     let surveyId = req.body.data.surveyId;
     let submittedTime = moment().format();
     let submittedEpoch = new Date().getTime();
-
-    for (let i = 0; i < answers.length; i++) {
-        answers[i].submittedTime = submittedTime
-        answers[i].submittedEpoch = submittedEpoch
+    console.log(answers.length)
+    if (answers.length === 0) {
+        answers[0] = {
+            submittedTime: submittedTime,
+            submittedEpoch: submittedEpoch
+        }
     }
-
+    else {
+        for (let i = 0; i < answers.length; i++) {
+            answers[i].submittedTime = submittedTime
+            answers[i].submittedEpoch = submittedEpoch
+        }
+    }
+    console.log(answers);
     const data = db.collection('projects');
-
     db.createCollection(surveyId + "::answers", {autoIndexId: false})
         .then((res) => {
             db.collection(surveyId + "::answers").insertMany(answers)
@@ -37,6 +44,7 @@ function submitAnswers(req, db, cb) {
                                     })
                                 })
                                 .catch((err) => {
+                                    console.log("1", err)
                                     cb({
                                         code: 400,
                                         message: "Error in submitting answers"
@@ -44,6 +52,7 @@ function submitAnswers(req, db, cb) {
                                 })
                         })
                         .catch((err) => {
+                            console.log("2", err)
                             cb({
                                 code: 400,
                                 message: "Error in submitting answers"
@@ -51,6 +60,7 @@ function submitAnswers(req, db, cb) {
                         })
                 })
                 .catch((err) => {
+                    console.log("3", err)
                     cb({
                         code: 400,
                         message: "Error in submitting answers"
