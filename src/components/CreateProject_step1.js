14055431294040cb94958fc59f9292fa8b668098
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-18 20:55:43 -0400

    change UI for multiple versions of MAUQ

--- src/components/CreateProject_step1.js
+++ src/components/CreateProject_step1.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {Select} from 'antd';
+import {Radio} from 'antd';
 
+const RadioGroup = Radio.Group;
 const Option = Select.Option;
 
 class CreateProjectStep1 extends Component {
     componentWillMount = () => {
         this.setState({
-            template: 1
+            template: 1,
+            options1: false,
+            options2: false
         });
     }
 
@@ -14,15 +18,19 @@ class CreateProjectStep1 extends Component {
         if (this.props.props.data.step1) {
             console.log("inside if");
             document.getElementById("project_name").value = this.props.props.data.step1.project_name || ""
+            this.setState({template: 1})
             document.getElementById("project_description").value = this.props.props.data.step1.description || ""
         }
     }
 
     next = () => {
+        console.log(this.state.template)
         let projectName = document.getElementById("project_name").value
         let projectDescription = document.getElementById("project_description").value
         if (projectName.trim().length <= 0) {
             alert("Please enter project name");
+        } else if (this.state.template == 0 || this.state.template == 2 || this.state.template == 3) {
+            alert("Please select type of questionnaire")
         } else {
             let step = {
                 step1: {
@@ -38,10 +46,39 @@ class CreateProjectStep1 extends Component {
     }
 
 
-    onChange = (type) => {
-        this.setState({
-            template: type.key
-        });
+    onChange = (e) => {
+        let value = e.target.value
+        //1 = TUQ
+        if (value === 1) {
+            this.setState({
+                template: value,
+                options1: false,
+                options2: false
+            });
+        } else if (value === 0) {   //0 = MUQ
+            this.setState({
+                template: value,
+                options1: true, //Provider or patient
+                options2: false //interactive or standalone
+            });
+        } else if (value === 2 || value === 3) {    //provider or patient values
+            if (this.state.template > value) {
+                this.setState({
+                    options1: true,
+                    options2: true
+                });
+            } else {
+                this.setState({
+                    template: value,
+                    options1: true,
+                    options2: true
+                });
+            }
+        } else {        //interactive or standalone
+            this.setState({
+                template: value
+            })
+        }
     }
 
     render = () => {
@@ -63,15 +100,34 @@ class CreateProjectStep1 extends Component {
                 </div>
                 <div style={{padding: "10px"}}>
                     <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
-                    style={{color: "white", fontWeight: "bold", marginTop: "10px"}}>Please select default
+                    style={{color: "white", marginTop: "10px"}}>Please select default
                     questionnaire</label><br/>
-                    <Select labelInValue style={{width: 120, backgroundColor: "white"}} onChange={this.onChange}>
-                        <Option value="1">TUQ</Option>
-                        <Option value="2">MAUQ Patient Interactive</Option>
-                        <Option value="3">MAUQ Provider Interactive</Option>
-                        <Option value="4">MAUQ Patient Standalone</Option>
-                        <Option value="5">MAUQ Provider Interactive</Option>
-                    </Select>
+                    <RadioGroup onChange={this.onChange} value={this.state.template}>
+                        <Radio value={1} style={{color: "white"}}>TUQ</Radio>
+                        <Radio value={0} style={{color: "white"}}>MAUQ</Radio>
+                    </RadioGroup>
+                    {
+                        (this.state.options1) ? <div style={{"marginTop": "10px"}}>
+                            <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
+                            style={{color: "white", marginTop: "10px"}}>Please select type of
+                            MAUQ</label><br/>
+                            <RadioGroup onChange={this.onChange}>
+                                <Radio value={2} style={{color: "white"}}>Patient</Radio>
+                                <Radio value={3} style={{color: "white"}}>Provider</Radio>
+                            </RadioGroup>
+                        </div> : ""
+                    }
+                    {
+                        (this.state.options2) ? <div style={{"marginTop": "10px"}}>
+                            <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
+                            style={{color: "white", marginTop: "10px"}}>Please select type of
+                            MAUQ</label><br/>
+                            <RadioGroup onChange={this.onChange}>
+                                <Radio value={4} style={{color: "white"}}>Standalone</Radio>
+                                <Radio value={5} style={{color: "white"}}>Interactive</Radio>
+                            </RadioGroup>
+                        </div> : ""
+                    }
                 </div>
                 <input type={"submit"} style={{width: "42%", marginTop: "10px", padding: "10px"}} value={"Continue"}
                        onClick={this.next}/>
