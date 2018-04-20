a45759a6d5874d78d355f3980f7e9ddc51ce24a5
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-20 19:25:18 -0400

    change option of other,please add to just others for demographic question

--- apis/models/demographic.js
+++ apis/models/demographic.js
@@ -1,20 +1,20 @@
 const demoQuestions = [
     {
-        question:"What is your gender?",
-        options:["Male", "Female"],
-        type:"radio",
-        questionId:"V6FWGB6ADU"
+        question: "What is your gender?",
+        options: ["Male", "Female"],
+        type: "radio",
+        questionId: "V6FWGB6ADU"
     },
     {
-        question:"What is your age?",
-        type:"text",
-        questionId:"OE7HKFG6YJ"
+        question: "What is your age?",
+        type: "text",
+        questionId: "OE7HKFG6YJ"
     },
     {
-        question:"What is your race?",
-        options:["Black", "White", "Asian", "Pacific Islander", "Mixed Race", "Other, please list"],
-        type:"radio",
-        questionId:"BP3I5XT87N"
+        question: "What is your race?",
+        options: ["Black", "White", "Asian", "Pacific Islander", "Mixed Race", "Other"],
+        type: "radio",
+        questionId: "BP3I5XT87N"
     }
 ]
 
