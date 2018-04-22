9372ead8ede519208ad8c1e0d576e49389306ec8
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-21 20:27:47 -0400

    change index route to about page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import {Router, Route, IndexRoute} from "react-router";
 import AnswersSubmitConfirmation from './pages/AnswersSubmitConfirmation'
 import CreateProject from './pages/CreateProject'
 import ListProjects from './pages/ListProjects'
-import Fillanswers from './pages/Fillanswers'
 import Fillanswershome from './pages/Fillanswershome'
 import About from './components/About'
 import References from './components/References'
@@ -21,8 +20,8 @@ ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
             <Route path="/survey_system/" component={Homepage}>
+                <IndexRoute component={About}/>
                 <Route path="/survey_system/login" component={Login}/>
-                {/*<IndexRoute component={ListProjects}/>*/}
                 <Route path="/survey_system/about" component={About}/>
                 <Route path="/survey_system/references" component={References}/>
                 <Route path="/survey_system/list_projects" component={ListProjects}/>
