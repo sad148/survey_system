cccf03e4167e81a0ee86c5908fbdd39eaf7636de
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-25 00:31:34 -0400

    add more countries

--- apis/models/countries.js
+++ apis/models/countries.js
@@ -1,14 +1,128 @@
 function countries() {
-    return ([{"id": "afghanistan", "value": "Afghanistan"}, {"id": "albania", "value": "Albania"}, {
-        "id": "algeria",
-        "value": "Algeria"
-    }, {"id": "americansamoa", "value": "American Samoa"}, {"id": "andorra", "value": "Andorra"}, {
-        "id": "angola",
-        "value": "Angola"
-    }, {"id": "anguilla", "value": "Anguilla"}, {"id": "antarctica", "value": "Antarctica"}, {
-        "id": "antigua&barbuda",
-        "value": "Antigua & Barbuda"
-    }, {"id": "argentina", "value": "Argentina"}])
+    return ([
+        {
+            "id": "argentina",
+            "value": "Argentina"
+        },
+        {
+            "id": "australia",
+            "value": "Australia"
+        }, {
+            "id": "austria",
+            "value": "Austria"
+        }, {
+            "id": "belgium",
+            "value": "Belgium"
+        }, {
+            "id": "brazil",
+            "value": "Brazil"
+        }, {
+            "id": "canada",
+            "value": "Canada"
+        }, {
+            "id": "czechrepublic",
+            "value": "Czech Republic"
+        }, {
+            "id": "denmark",
+            "value": "Denmark"
+        }, {
+            "id": "egypt",
+            "value": "Egypt"
+        }, {
+            "id": "finland",
+            "value": "Finland"
+        }, {
+            "id": "france",
+            "value": "France"
+        }, {
+            "id": "germany",
+            "value": "Germany"
+        }, {
+            "id": "greece",
+            "value": "Greece"
+        }, {
+            "id": "hongkong",
+            "value": "Hong Kong"
+        }, {
+            "id": "india",
+            "value": "India"
+        }, {
+            "id": "iran",
+            "value": "Iran"
+        }, {
+            "id": "ireland",
+            "value": "Ireland"
+        }, {
+            "id": "israel",
+            "value": "Israel"
+        }, {
+            "id": "italy",
+            "value": "Italy"
+        }, {
+            "id": "japan",
+            "value": "Japan"
+        }, {
+            "id": "malaysia",
+            "value": "Malaysia"
+        }, {
+            "id": "mexico",
+            "value": "Mexico"
+        }, {
+            "id": "netherlands",
+            "value": "Netherlands"
+        }, {
+            "id": "newzealand",
+            "value": "New Zealand"
+        }, {
+            "id": "norway",
+            "value": "Norway"
+        }, {
+            "id": "poland",
+            "value": "Poland"
+        }, {
+            "id": "portugal",
+            "value": "Portugal"
+        }, {
+            "id": "romania",
+            "value": "Romania"
+        }, {
+            "id": "russia",
+            "value": "Russia"
+        }, {
+            "id": "singapore",
+            "value": "Singapore"
+        }, {
+            "id": "southafrica",
+            "value": "South Africa"
+        }, {
+            "id": "southkorea",
+            "value": "South Korea"
+        }, {
+            "id": "spain",
+            "value": "Spain"
+        }, {
+            "id": "sweden",
+            "value": "Sweden"
+        }, {
+            "id": "switzerland",
+            "value": "Switzerland"
+        }, {
+            "id": "taiwan",
+            "value": "Taiwan"
+        }, {
+            "id": "thailand",
+            "value": "Thailand"
+        }, {
+            "id": "turkey",
+            "value": "Turkey"
+        }, {
+            "id": "unitedkingdom",
+            "value": "United Kingdom"
+        }, {
+            "id": "unitedstates",
+            "value": "United States"
+        }
+    ])
 }
 
 module.exports.countries = countries
