fa3841795a1bf96d1f32d1c32b3b81c732315cad
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-04-28 20:45:25 -0700

    fix issue of selecting wrong version of muq

--- src/components/CreateProject_step1.js
+++ src/components/CreateProject_step1.js
@@ -8,33 +8,31 @@ const Option = Select.Option;
 class CreateProjectStep1 extends Component {
     componentWillMount = () => {
         this.setState({
-            template: 1,
-            options1: false,
-            options2: false
+            template: "tuq",
+            showOptions: "",
+            showTypes: false
         });
     }
 
     componentDidMount = () => {
         if (this.props.props.data.step1) {
-            console.log("inside if");
             document.getElementById("project_name").value = this.props.props.data.step1.project_name || ""
-            this.setState({template: 1})
+            this.setState({template: "tuq"})
             document.getElementById("project_description").value = this.props.props.data.step1.description || ""
         }
     }
 
     next = () => {
-        console.log(this.state.template)
         let projectName = document.getElementById("project_name").value
         let projectDescription = document.getElementById("project_description").value
         if (projectName.trim().length <= 0) {
             alert("Please enter project name");
-        } else if (this.state.template == 0 || this.state.template == 2 || this.state.template == 3) {
+        } else if (this.state.template == "mauq") {
             alert("Please select type of questionnaire")
         } else {
             let step = {
                 step1: {
-                    template: this.state.template,
+                    template: this.state.template === "tuq" ? 1 : this.state.template,
                     project_name: projectName,
                     description: projectDescription
                 }
@@ -46,42 +44,49 @@ class CreateProjectStep1 extends Component {
     }
 
 
-    onChange = (e) => {
+    selectOptionsSet = (e) => {
         let value = e.target.value
-        //1 = TUQ
-        if (value === 1) {
-            this.setState({
-                template: value,
-                options1: false,
-                options2: false
-            });
-        } else if (value === 0) {   //0 = MUQ
-            this.setState({
-                template: value,
-                options1: true, //Provider or patient
-                options2: false //interactive or standalone
-            });
-        } else if (value === 2 || value === 3) {    //provider or patient values
-            if (this.state.template > value) {
-                this.setState({
-                    options1: true,
-                    options2: true
-                });
-            } else {
-                this.setState({
-                    template: value,
-                    options1: true,
-                    options2: true
-                });
-            }
-        } else {        //interactive or standalone
-            this.setState({
-                template: value
-            })
-        }
+        this.setState({
+            template: value
+        })
     }
 
+    changeOptions = (e) => {
+        let value = e.target.value
+        if (value === "tuq") {
+            this.setState({
+                template: value,
+                showTypes: false,
+                showOptions: ""
+            });
+        }
+        else if (value === "mauq") {
+            this.setState({
+                template: value,
+                showTypes: true
+            });
+        } else if (value === "patients") {
+            this.setState({showOptions: "patients", template: 4})
+        } else {
+            this.setState({showOptions: "provider", template: 5})
+        }
+    }
     render = () => {
+        let patientOptions = (
+            <RadioGroup onChange={this.selectOptionsSet} value={this.state.template}>
+                <Radio value={4} style={{color: "white"}}>Standalone</Radio>
+                <Radio value={2} style={{color: "white"}}>Interactive</Radio>
+            </RadioGroup>
+        )
+
+        let providerOptions = (
+            <RadioGroup onChange={this.selectOptionsSet} value={this.state.template}>
+                <Radio value={5} style={{color: "white"}}>Standalone</Radio>
+                <Radio value={3} style={{color: "white"}}>Interactive</Radio>
+            </RadioGroup>
+        )
+
+
         return (
             <div style={{paddingLeft: "20%", paddingRight: "25%", marginTop: "10px"}}>
                 <div style={{padding: "10px"}}>
@@ -101,32 +106,57 @@ class CreateProjectStep1 extends Component {
                 <div style={{padding: "10px"}}>
                     <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
                     style={{color: "white", marginTop: "10px"}}>Please select the type of your system</label><br/>
-                    <RadioGroup onChange={this.onChange} value={this.state.template}>
-                        <Radio value={1} style={{color: "white"}}>Telehealth System(TUQ)</Radio>
-                        <Radio value={0} style={{color: "white"}}>Mobile Health App(MAUQ)</Radio>
+                    <RadioGroup onChange={this.changeOptions} value={this.state.template}>
+                        <Radio value={"tuq"} style={{color: "white"}}>Telehealth System(TUQ)</Radio>
+                        <Radio value={"mauq"} style={{color: "white"}}>Mobile Health App(MAUQ)</Radio>
                     </RadioGroup>
                     {
-                        (this.state.options1) ? <div style={{"marginTop": "10px"}}>
+                        (this.state.showTypes) ? <div style={{"marginTop": "10px"}}>
                             <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
-                            style={{color: "white", marginTop: "10px"}}>Please select the primary user of the mobile
-                            health app</label><br/>
-                            <RadioGroup onChange={this.onChange}>
-                                <Radio value={2} style={{color: "white"}}>Patient</Radio>
-                                <Radio value={3} style={{color: "white"}}>Provider</Radio>
+                            style={{color: "white", marginTop: "10px"}}>Please select the primary user of the
+                            mobile</label><br/>
+                            <RadioGroup onChange={this.changeOptions}>
+                                <Radio value={"patients"} style={{color: "white"}}>Patient</Radio>
+                                <Radio value={"provider"} style={{color: "white"}}>Provider</Radio>
                             </RadioGroup>
                         </div> : ""
                     }
                     {
-                        (this.state.options2) ? <div style={{"marginTop": "10px"}}>
-                            <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
-                            style={{color: "white", marginTop: "10px"}}>Please select the type of the mobile health
-                            app</label><br/>
-                            <RadioGroup onChange={this.onChange}>
-                                <Radio value={4} style={{color: "white"}}>Standalone</Radio>
-                                <Radio value={5} style={{color: "white"}}>Interactive</Radio>
-                            </RadioGroup>
-                        </div> : ""
+                        (this.state.showOptions == "patients") ?
+                            <div style={{"marginTop": "10px"}}>
+                                <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
+                                style={{color: "white", marginTop: "10px"}}>Please select the type of the mobile
+                                health</label><br/>
+                                {patientOptions}
+
+                            </div>
+                            : (this.state.showOptions == "provider") ?
+                            <div style={{"marginTop": "10px"}}>
+                                <label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label
+                                style={{color: "white", marginTop: "10px"}}>Please select the type of the mobile
+                                health</label><br/>
+                                {providerOptions}
+                            </div> : ""
                     }
+                    {/*{*/}
+                    {/*(this.state.optionsq) ? <div style={{"marginTop": "10px"}}>*/}
+                    {/*<label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label*/}
+                    {/*style={{color: "white", marginTop: "10px"}}>Please select the primary user of the mobile*/}
+                    {/*health app</label><br/>*/}
+                    {/*<RadioGroup onChange={this.onChange}>*/}
+                    {/*<Radio value={2} style={{color: "white"}}>Patient</Radio>*/}
+                    {/*<Radio value={3} style={{color: "white"}}>Provider</Radio>*/}
+                    {/*</RadioGroup>*/}
+                    {/*</div> : ""*/}
+                    {/*}*/}
+                    {/*{*/}
+                    {/*(this.state.options2) ? <div style={{"marginTop": "10px"}}>*/}
+                    {/*<label style={{color: "red", marginTop: "10px"}}>*</label>&nbsp;<label*/}
+                    {/*style={{color: "white", marginTop: "10px"}}>Please select the type of the mobile health*/}
+                    {/*app</label><br/>*/}
+
+                    {/*</div> : ""*/}
+                    {/*}*/}
                 </div>
                 <input type={"submit"} style={{width: "42%", marginTop: "10px", padding: "10px"}} value={"Continue"}
                        onClick={this.next}/>
