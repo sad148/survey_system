3bad4980de5de6f8c59d3e3f35e0ed2d7fcfa656
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-05-12 23:40:30 -0700

    Update admin password script

--- apis/insertAdminInDB.js
+++ apis/insertAdminInDB.js
@@ -1,21 +1,22 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-
+var md5 = require('md5')
 MongoClient.connect(url, function (err, db) {
+    let password = "survey_admin"
     if (err) throw err;
     var dbo = db.db("survey_system");
     var myobj = {
         "username": "admin",
-        "password": "d0f3a0e129ba85460020433be9bc191a",
+        "password": md5(password),
         "first_name": "Admin",
         "last_name": "",
         "email": "admin",
         "user_id": "b8451d86431311e8842f0ed5f89f718b"
     }
-    dbo.collection("users").insertOne(myobj, function (err, res) {
+    dbo.collection("users").updateOne({user_id: "b8451d86431311e8842f0ed5f89f718b"}, {'$set': myobj}, function (err, res) {
         if (err) throw err;
         console.log("1 document inserted");
-        db.close();
-    });
+        process.exit();
+    })
 });
 
