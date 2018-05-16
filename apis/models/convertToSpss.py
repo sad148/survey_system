5ad4e5d0c474893592788d502b7a4c787233c132
Saurabh Dhamnaskar <sad148@pitt.edu>
2018-05-15 21:49:13 -0700

    Update save filepath

--- apis/models/convertToSpss.py
+++ apis/models/convertToSpss.py
@@ -3,7 +3,7 @@ from savReaderWriter.savWriter import *;
 
 file1 = open(sys.argv[2])
 data = json.loads(file1.read())
-savFileName = "./apis/exports/" + sys.argv[1];
+savFileName = "./exports/" + sys.argv[1];
 varNames = data["varNames"]["varNames"];
 records = data["records"]["records"];
 varTypes = data["varTypes"]
